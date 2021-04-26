0a287011798ccac7d5f49b27a712cf85567e5b12
hiagobchaves <hiago.bchaves@gmail.com>
2021-04-26 14:59:10 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'truechangechallenge'`, () => {
+  it(`should have as title 'trueChange-Challenge'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('truechangechallenge');
+    expect(app.title).toEqual('trueChange-Challenge');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('truechangechallenge app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('trueChange-Challenge app is running!');
   });
 });
